b6820a742e387f4538f6ebbeb12e34b6d3c16f2f
Yasu <yasu@fuchslativ.de>
2023-11-17 09:52:24 +0100

    :bug: Fix array key doesn't exist warning in VulnerableBackendDependenciesCheck

--- src/Checks/Dependencies/VulnerableBackendDependenciesCheck.php
+++ src/Checks/Dependencies/VulnerableBackendDependenciesCheck.php
@@ -39,21 +39,25 @@ class VulnerableBackendDependenciesCheck extends AbstractDependenciesCheck
             true
         );
 
-        foreach ($response['advisories'] as $package => $advisories) {
-            $messages = [sprintf('- Vulnerable package <fail>%s</fail> found', $package)];
+        if (array_key_exists('advisories', $response)) {
+            foreach ($response['advisories'] as $package => $advisories) {
+                $messages = [sprintf('- Vulnerable package <fail>%s</fail> found', $package)];
 
-            foreach ($advisories as $advisory) {
-                $messages[] = sprintf('  <fail>%s</fail>: %s', $advisory['cve'], $advisory['link']);
+                foreach ($advisories as $advisory) {
+                    $messages[] = sprintf('  <fail>%s</fail>: %s', $advisory['cve'], $advisory['link']);
+                }
+
+                $this->fail($messages);
             }
-
-            $this->fail($messages);
         }
 
-        foreach ($response['abandoned'] as $package => $alternative) {
-            $this->warn([
-                sprintf('- Abandoned package <warn>%s</warn> found', $package),
-                sprintf('  possible replacement: <mark>%s</mark>', $alternative)
-            ]);
+        if (array_key_exists('abandoned', $response)) {
+            foreach ($response['abandoned'] as $package => $alternative) {
+                $this->warn([
+                    sprintf('- Abandoned package <warn>%s</warn> found', $package),
+                    sprintf('  possible replacement: <mark>%s</mark>', $alternative)
+                ]);
+            }
         }
 
         if (!empty($response['advisories']) || !empty($response['abandoned'])) {
